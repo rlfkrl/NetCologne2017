12b64d829b4f856a4852e4af9b60bbfc2a2d4906
Ralf Karle <ralf@ralfkarle.de>
2017-04-28 15:54:33 +0200

    restart appPool build all projects

--- src/ASP.NET And Core Share Cookie/publish.ps1
+++ src/ASP.NET And Core Share Cookie/publish.ps1
@@ -48,12 +48,20 @@ if (-not (Test-Path $idsAppName -pathType container))
 	New-Item -Path $dir -ItemType Directory
 	New-WebApplication -Name $idsAppName -ApplicationPool $idsAppPoolName -physicalPath $dir -Site $webSite
 }
+else
+{
+	Restart-WebAppPool -Name $idsAppPoolName
+}
 
 if (-not (Test-Path $aspNetAppName -pathType container))
 {
 	$dir = Join-Path $directoryPath $aspNetAppName
 	New-Item -Path $dir -ItemType Directory
 	New-WebApplication -Name $aspNetAppName -ApplicationPool $aspNetAppPoolName -physicalPath $dir -Site $webSite
+}
+else
+{
+	Restart-WebAppPool -Name $aspNetAppPoolName
 }
 
 if (-not (Test-Path $aspNetCoreAppName -pathType container))
@@ -62,4 +70,20 @@ if (-not (Test-Path $aspNetCoreAppName -pathType container))
 	New-Item -Path $dir -ItemType Directory
 	New-WebApplication -Name $aspNetCoreAppName -ApplicationPool $aspNetCoreAppPoolName -physicalPath $dir -Site $webSite
 }
+else
+{
+	Restart-WebAppPool -Name $aspNetCoreAppPoolName 
+}
 
+# publish the project
+ $msbuild = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\msbuild.exe"
+ $solutionDir = $PSScriptRoot
+
+ $project = join-path $solutionDir "AspNetCoreWebSite\AspNetCoreWebSite.csproj"
+ & $msbuild "$project" /p:DeployOnBuild=true /p:PublishProfile=FolderProfile
+
+ $project = join-path $solutionDir "AspNetWebSite\AspNetWebSite.csproj"
+ & $msbuild "$project" /p:DeployOnBuild=true /p:PublishProfile=FolderProfile
+
+ $project = join-path $solutionDir "IdentityServer\IdentityServer.csproj"
+ & $msbuild "$project" /p:DeployOnBuild=true /p:PublishProfile=FolderProfile
